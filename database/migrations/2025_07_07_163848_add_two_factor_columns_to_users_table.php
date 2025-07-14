d4f1f0c0a6918a4725ce6f1d597cb236175c2c0d
Rowan Christmas <rowanxmas@gmail.com>
2025-07-13 23:04:10 -0700

    duplication

--- database/migrations/2025_07_07_163848_add_two_factor_columns_to_users_table.php
+++ database/migrations/2025_07_07_163848_add_two_factor_columns_to_users_table.php
@@ -12,10 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                ->after('password')
-                ->nullable();
-
             $table->text('two_factor_recovery_codes')
                 ->after('two_factor_secret')
                 ->nullable();
