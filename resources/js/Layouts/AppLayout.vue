b814735633930df6103d684d7a390b5754298bdc
Rowan Christmas <rowanxmas@gmail.com>
2025-07-13 23:30:50 -0700

    admins can see teams

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -91,30 +91,28 @@ const logout = () => {
 
                                             <DropdownLink v-if="$page.props.jetstream.canCreateTeams" :href="route('teams.create')">
                                                 Create New Team
-                                            </DropdownLink>
+                                            </DropdownLink>                            <!-- Team Switcher -->
+                            <template v-if="$page.props.auth.user.all_teams.length > 1 || $page.props.auth.user.is_admin">
+                                <div class="border-t border-gray-200 dark:border-gray-600" />
 
-                                            <!-- Team Switcher -->
-                                            <template v-if="$page.props.auth.user.all_teams.length > 1">
-                                                <div class="border-t border-gray-200 dark:border-gray-600" />
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    Switch Teams
+                                </div>
 
-                                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                                    Switch Teams
-                                                </div>
+                                <template v-for="team in $page.props.auth.user.all_teams" :key="team.id">
+                                    <form @submit.prevent="switchToTeam(team)">
+                                        <DropdownLink as="button">
+                                            <div class="flex items-center">
+                                                <svg v-if="team.id == $page.props.auth.user.current_team_id" class="me-2 size-5 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                </svg>
 
-                                                <template v-for="team in $page.props.auth.user.all_teams" :key="team.id">
-                                                    <form @submit.prevent="switchToTeam(team)">
-                                                        <DropdownLink as="button">
-                                                            <div class="flex items-center">
-                                                                <svg v-if="team.id == $page.props.auth.user.current_team_id" class="me-2 size-5 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                                </svg>
-
-                                                                <div>{{ team.name }}</div>
-                                                            </div>
-                                                        </DropdownLink>
-                                                    </form>
-                                                </template>
-                                            </template>
+                                                <div>{{ team.name }}</div>
+                                            </div>
+                                        </DropdownLink>
+                                    </form>
+                                </template>
+                            </template>
                                         </div>
                                     </template>
                                 </Dropdown>
@@ -257,7 +255,7 @@ const logout = () => {
                                 </ResponsiveNavLink>
 
                                 <!-- Team Switcher -->
-                                <template v-if="$page.props.auth.user.all_teams.length > 1">
+                                <template v-if="$page.props.auth.user.all_teams.length > 1 || $page.props.auth.user.is_admin">
                                     <div class="border-t border-gray-200 dark:border-gray-600" />
 
                                     <div class="block px-4 py-2 text-xs text-gray-400">
