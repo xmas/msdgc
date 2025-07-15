3e3fb28b3f3ddb4def7b80fd4a80b2ad8def42c6
Rowan Christmas <rowanxmas@gmail.com>
2025-07-15 16:47:17 -0700

    old board minutes

--- resources/js/Pages/Board.vue
+++ resources/js/Pages/Board.vue
@@ -56,7 +56,7 @@ const cardStyle = "flex flex-col items-start gap-6 overflow-hidden rounded-lg p-
                                 <div class="box-page">
                                     <div class="card-header">
                                         <span class="card-header-icon">
-                                            <svg class="w-8 h-8 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                                             </svg>
                                         </span>
@@ -92,7 +92,7 @@ const cardStyle = "flex flex-col items-start gap-6 overflow-hidden rounded-lg p-
                                 <div class="box-page w-full">
                                     <div class="card-header">
                                         <span class="card-header-icon">
-                                            <svg class="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                                             </svg>
                                         </span>
@@ -139,15 +139,15 @@ const cardStyle = "flex flex-col items-start gap-6 overflow-hidden rounded-lg p-
                                                         </div>
                                                     </div>
 
-                                                    <div v-if="minute.content" class="text-gray-700 text-sm line-clamp-3">
+                                                    <!-- <div v-if="minute.content" class="text-gray-700 text-sm line-clamp-3">
                                                         {{ minute.content.substring(0, 200) }}{{ minute.content.length > 200 ? '...' : '' }}
-                                                    </div>
+                                                    </div> -->
                                                 </div>
 
                                                 <div class="ml-4 flex-shrink-0">
                                                     <Link
                                                         :href="route('board.minutes.show', minute.id)"
-                                                        class="btn-primary"
+                                                        class="btn-primary bangers text-3xl"
                                                     >
                                                         View Minutes
                                                     </Link>
